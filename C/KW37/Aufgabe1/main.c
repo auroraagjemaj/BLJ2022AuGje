5082e38603c55a372decdf557f5c486043c06945
Aurora Gjemaj <aurora.gjemaj@noseryoung.com>
2022-09-15 15:51:40 +0200

    new file:   C/KW37/Aufgabe1/.dep.inc 	new file:   C/KW37/Aufgabe1/Makefile 	new file:   C/KW37/Aufgabe1/build/Debug/MinGW-Windows/main.o 	new file:   C/KW37/Aufgabe1/build/Debug/MinGW-Windows/main.o.d 	new file:   C/KW37/Aufgabe1/dist/Debug/MinGW-Windows/aufgabe1.exe 	new file:   C/KW37/Aufgabe1/main.c 	new file:   C/KW37/Aufgabe1/nbproject/Makefile-Debug.mk 	new file:   C/KW37/Aufgabe1/nbproject/Makefile-Release.mk 	new file:   C/KW37/Aufgabe1/nbproject/Makefile-impl.mk 	new file:   C/KW37/Aufgabe1/nbproject/Makefile-variables.mk 	new file:   C/KW37/Aufgabe1/nbproject/Package-Debug.bash 	new file:   C/KW37/Aufgabe1/nbproject/Package-Release.bash 	new file:   C/KW37/Aufgabe1/nbproject/configurations.xml 	new file:   C/KW37/Aufgabe1/nbproject/private/Makefile-variables.mk 	new file:   C/KW37/Aufgabe1/nbproject/private/c_standard_headers_indexer.c 	new file:   C/KW37/Aufgabe1/nbproject/private/configurations.xml 	new file:   C/KW37/Aufgabe1/nbproject/private/cpp_standard_headers_indexer.cpp 	new file:   C/KW37/Aufgabe1/nbproject/private/launcher.properties 	new file:   C/KW37/Aufgabe1/nbproject/private/private.xml 	new file:   C/KW37/Aufgabe1/nbproject/project.xml 	new file:   C/KW37/aufgabe2/.dep.inc 	new file:   C/KW37/aufgabe2/Makefile 	new file:   C/KW37/aufgabe2/build/Debug/MinGW-Windows/main.o 	new file:   C/KW37/aufgabe2/build/Debug/MinGW-Windows/main.o.d 	new file:   C/KW37/aufgabe2/dist/Debug/MinGW-Windows/aufgabe2.exe 	new file:   C/KW37/aufgabe2/main.c 	new file:   C/KW37/aufgabe2/nbproject/Makefile-Debug.mk 	new file:   C/KW37/aufgabe2/nbproject/Makefile-Release.mk 	new file:   C/KW37/aufgabe2/nbproject/Makefile-impl.mk 	new file:   C/KW37/aufgabe2/nbproject/Makefile-variables.mk 	new file:   C/KW37/aufgabe2/nbproject/Package-Debug.bash 	new file:   C/KW37/aufgabe2/nbproject/Package-Release.bash 	new file:   C/KW37/aufgabe2/nbproject/configurations.xml 	new file:   C/KW37/aufgabe2/nbproject/private/Makefile-variables.mk 	new file:   C/KW37/aufgabe2/nbproject/private/c_standard_headers_indexer.c 	new file:   C/KW37/aufgabe2/nbproject/private/configurations.xml 	new file:   C/KW37/aufgabe2/nbproject/private/cpp_standard_headers_indexer.cpp 	new file:   C/KW37/aufgabe2/nbproject/private/launcher.properties 	new file:   C/KW37/aufgabe2/nbproject/private/private.xml 	new file:   C/KW37/aufgabe2/nbproject/project.xml 	new file:   C/KW38/Textdokument (neu).txt 	new file:   JAVA/Textdokument (neu).txt 	new file:   gitignore

---
+++ C/KW37/Aufgabe1/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Aurora
+ *
+ * Created on 15. September 2022, 10:59
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Bahnhofstrasse 268, Wetzikon 8623 Schweiz, 0787781166\n");
+    return (EXIT_SUCCESS);
+}
+
---
+++ C/KW37/aufgabe2/main.c
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Aurora
+ *
+ * Created on 15. September 2022, 11:13
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+  /*  float f = 5.67;
+    
+    
+printf("Speicherbedarf int: %d Byte(s)\n", sizeof(int));
+printf("Speicherbedarf float: %d Byte(s)\n", sizeof(float));
+printf("Speicherbedarf double: %d Byte(s)\n", sizeof(double));
+printf("Speicherbedarf char: %d Byte(s)\n", sizeof(char)); */
+    
+   /* printf("WERTE DER VARIABLEN\n");
+    printf("------------------------------------------------------\n");
+    printf("Variable    Zeichen     dez     hex     okt     \n");
+    printf("------------------------------------------------------\n");
+    printf("more..");*/
+    
+   /* int number= 1000;
+    printf("%d\n", number);
+    printf("%o\n", number);
+    printf("%x\n", number); */
+   
+    /*float radius = 4; 
+    float extent = radius*radius*3.14;
+    
+    printf("Umfang des Kreises %f" , extent); */
+    
+    int h = 0xff;
+    unsigned char o = 23;
+    printf("%d\n", h );
+    printf("%d\n", o);
+    
+    return (EXIT_SUCCESS);
+}
+
