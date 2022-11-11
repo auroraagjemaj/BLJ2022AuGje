3cb9edc2ed2baed4f09a46bd9e437a7a349c93a6
auroraagjemaj <aurora.gjemaj@noseryoung.com>
2022-11-11 21:15:27 +0100

    finished project

---
+++ JAVA/KW45/untitled/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+                int summ = 0;
+                for (int i = 3; i < 28; i++){
+                    summ = summ + i;
+                }
+        System.out.println(summ);
+
+            }
+        }
+                
